c0d2b40b2b3c363928391e974e43482b53ce8c34
jroblesluna <jrobles@sistemas.com.pe>
2023-06-25 16:49:08 -0500

    Finished

--- screens/Semana16.tsx
+++ screens/Semana16.tsx
@@ -5,6 +5,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {CartScreen} from './CartScreen';
 import {AddProductScreen} from './AddProductScreen';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {StyleSheet, Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
 
 interface Props extends NativeStackScreenProps<StackRootParams, 'Semana16'> {}
 
@@ -16,9 +18,24 @@ export type TabRootParams = {
 const Tab = createBottomTabNavigator<TabRootParams>();
 
 const AddProductIcon = () => <Icon name="plus" size={30} color="black" />;
-const CartIcon = () => <Icon name="shopping-cart" size={30} color="black" />;
+//const CartIcon = () => <Icon name="shopping-cart" size={30} color="black" />;
+const CartIconWithBadge = ({count}) => {
+  const digitCount = count.toString().length;
+  const badgeWidth = 15 * digitCount;
+  return (
+    <View style={{position: 'relative'}}>
+      <Icon name="shopping-cart" size={30} color="black" />
+      {count > 0 && (
+        <View style={[styles.badge, {width: badgeWidth}]}>
+          <Text style={styles.badgeText}>{count}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
 
 export const Semana16: React.FC<Props> = () => {
+  const cartItemsCount = useSelector(state => state.cart.items.length);
   return (
     <Tab.Navigator>
       <Tab.Screen
@@ -32,9 +49,29 @@ export const Semana16: React.FC<Props> = () => {
         name="CartScreen"
         component={CartScreen}
         options={{
-          tabBarIcon: () => <CartIcon />,
+          tabBarIcon: () => <CartIconWithBadge count={cartItemsCount} />,
         }}
       />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  badge: {
+    position: 'absolute',
+    //top: -5,
+    //right: -5,
+    left: 20,
+    backgroundColor: 'red',
+    width: 15,
+    height: 15,
+    borderRadius: 5,
+  },
+  badgeText: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: 'bold',
+    //paddingHorizontal: 5,
+    alignSelf: 'center',
+  },
+});
